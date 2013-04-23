6cebfb8d62c87258ed1fcfabe78c88e3b8f3b514
Sumin Byeon <suminb@gmail.com>
2013-04-23 02:32:35 -0700

    Minor improvements.

--- bin/fetch_feeds.py
+++ bin/fetch_feeds.py
@@ -1,6 +1,7 @@
 import sys, os
 sys.path.insert(0, os.path.abspath('.'))
 
+from sqlalchemy.exc import IntegrityError
 from app.core import db
 from app.models import *
 
@@ -20,6 +21,8 @@ def __tmp__():
 def add_feed(feed_url):
     # TODO: verify feed_url is in a valid format
 
+    print 'Adding a new feed: %s' % feed_url
+
     feed_url = feed_url.strip()
 
     fp = feedparser.parse(feed_url)
@@ -33,13 +36,21 @@ def add_feed(feed_url):
         description=fp['channel']['description'],
     )
 
-    db.session.add(feed)
-    db.session.commit()
+    try:
+        db.session.add(feed)
+        db.session.commit()
+
+    except IntegrityError as e:
+        print '%s already exists.' % feed_url
 
 def fetch_feed_items():
     """Parses a set of feeds and insert feed items into the database."""
-    
-    for feed in Feed.query.all():
+
+    for feed in Feed.query.filter(or_(
+            Feed.timestamp_fetched == None,
+            Feed.timestamp_fetched <= datetime.datetime.now() - datetime.timedelta(hours=1))
+        ).all():
+
         fp = feedparser.parse(feed.feed_url)
         timestamp_fetched = datetime.datetime.now()
 
@@ -47,23 +58,29 @@ def fetch_feed_items():
 
             print 'Processing "%s"' % item['link']
 
-            print item
+            feed_item = FeedItem.query.filter_by(link=item['link']).first()
 
-            feed_item = FeedItem(
-                id=str(uuid.uuid4()),
-                feed_id=feed.id,
-                timestamp_published=item['published'],
-                timestamp_fetched=timestamp_fetched,
-                title=item['title'],
-                link=item['link'],
-                description=item['summary'],
-            )
+            if feed_item != None:
+                print '%s already exists.' % feed_item.link
 
-            db.session.add(feed_item)
+            else:
+                feed_item = FeedItem(
+                    id=str(uuid.uuid4()),
+                    feed_id=feed.id,
+                    timestamp_published=item['published'],
+                    timestamp_fetched=timestamp_fetched,
+                    title=item['title'],
+                    link=item['link'],
+                    description=item['summary'],
+                )
 
+                db.session.add(feed_item)
+
+        feed.timestamp_fetched = timestamp_fetched
         db.session.commit()
 
 if __name__ == '__main__':
     #add_feed('http://feeds.bbci.co.uk/news/video_and_audio/news_front_page/rss.xml?edition=uk')
-
+    #add_feed('http://uanews.org/rss/campus-news')
+    add_feed('http://feeds.foxnews.com/foxnews/health')
     fetch_feed_items()
